29a161e07996a201d5932840360984926d09f713
Prachi Nayakal <121372605+prachinayakal@users.noreply.github.com>
2024-10-17 09:44:06 +0530

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,14 @@
+# Looping statement
+# For loop
+#Looping statements are used to repeat a block of code multiple times
+print("This is for loop: ")
+for i in range(5):
+    print(i)
+    
+# While loop
+# The while loop we can execute a set of statements as long as a condition is true.
+print("This is while loop:")
+count = 0
+while count < 5:
+    print(count)
+    count += 1
